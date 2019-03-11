18eff801e7dc30ea73fbf87758229285c796fb3e
Brandon Braun <brandonbraun653@gmail.com>
2019-03-10 19:47:15 -0500

    Submodule update. Added new VS project for creating python bindings to the RF24Phy driver

---
+++ dev/VisualStudio/PythonBindings/PythonBindings/chimeraPort.hpp
@@ -0,0 +1,18 @@
+/********************************************************************************
+* File Name:
+*   chimeraPort.hpp
+*
+* Description:
+*   Provides the default inheritance bindings for Chimera. In this particular
+*   case, this will cause Chimera to be compiled but not have any functioning
+*   behavior. 
+*
+* 2019 | Brandon Braun | brandonbraun653@gmail.com
+********************************************************************************/
+#pragma once
+#ifndef PLACEHOLDER_CHIMERA_PORT_HPP
+#define PLACEHOLDER_CHIMERA_PORT_HPP
+
+#include <Chimera/interface.hpp>
+
+#endif /* !PLACEHOLDER_CHIMERA_PORT_HPP */
